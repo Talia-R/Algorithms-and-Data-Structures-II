6d7df1ee72f1e98cf23b73e028eec0e86b00932a
Talia-R <natalia.1067@hotmail.com>
2026-02-17 17:18:29 -0300

    updated to eng

--- 1. Recursion/src/App.java
+++ 1. Recursion/src/App.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 
 public class App {
-    static Scanner entrada = new Scanner(System.in);
+    static Scanner input = new Scanner(System.in);
 
-    public static int lerInteiro(String s){
+    public static int readInt(String s){
         System.out.print("\n" + s);
-        return Integer.parseInt(entrada.nextLine());
+        return Integer.parseInt(input.nextLine());
     }
 
     public static String menu(){
@@ -14,42 +14,58 @@ public class App {
         s.append("\n1) Soma de números pares");
         s.append("\n2) Somar elementos de um vetor de doubles");
         s.append("\n3) Contar quantidade de repetições de um laço");
+        s.append("\nExtras");
+        s.append("\n4) Fatorial");
+
         return s.toString();
     }
 
-    public static int somarPares(int limite, int total){
+    public static int sumEven(int limit, int total){
 
-        if(limite > 0){
+        if(limit > 0){
                 // if(ePar(limite))
                     // total += limite; 
-            somarPares(--limite, total);
+            sumEven(--limit, total);
         }
 
-        return ePar(limite) ? total+= limite : total;
+        return isEven(limit) ? total+= limit : total;
     }
 
-    public static boolean ePar(int x){
+    public static boolean isEven(int x){
         return x % 2 == 0;
     }
 
+    public static int factorial(int x){
+        int total;
+        if(x == 1){
+           total = 1;
+        } else {
+            total = x * factorial(x-1);
+        }
+        return total;
+    }
     public static void main(String[] args) throws Exception {
+        int entry;
         
-        System.out.println(menu());
-        int escolha;
-
         do{
-            escolha = Integer.parseInt(entrada.nextLine());
-            switch(escolha){
+            System.out.println(menu());
+            entry = Integer.parseInt(input.nextLine());
+            switch(entry){
                 case 1 -> {
-                    int max = lerInteiro("Digite até qual número deseja a soma: ");
-                    int totalPares = 0;
-                    System.out.println(somarPares(max, totalPares));
+                    int max = readInt("Digite até qual número deseja a soma: ");
+                    int totalEven = 0;
+                    System.out.println(sumEven(max, totalEven));
+                }
+                case 4 -> {
+                    System.out.println(" --- Fatorial ---");
+                    int x = readInt("Qual fatorial deseja saber?: ");
+                    System.out.printf("O fatorial de %d é: %d.", x, factorial(x));
                 }
             }
 
-        } while(escolha != 0);  
+        } while(entry != 0);  
 
-        entrada.close();
+        input.close();
     }
     
 
